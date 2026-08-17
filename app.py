b1fe36e030e6f279680f48f3afcc116813ab125c
Ankit Sharma <ankitsharma1831ao@gmail.com>
2026-08-17 17:00:21 +0530

    model depriciated

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ except Exception as e:
 # 4) Groq LLM (LangChain Native for LangSmith Tracking)
 client = ChatGroq(
     api_key=os.environ.get("GROQ_API_KEY"),
-    model="llama-3.3-70b-versatile",
+    model="openai/gpt-oss-20b",
     temperature=0.1,
     max_tokens=300
 )
